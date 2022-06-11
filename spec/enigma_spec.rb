c99ebec9eb1077d78de66e45d536f7e4ea185523
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-11 14:35:14 -0500

    Method/Initializ Enigma method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,11 +1,11 @@
 require './spec_helper'
 
-RSpec.describe Engima do
+RSpec.describe Enigma do
   before :each do
     @enigma = Enigma.new
   end
 
   it 'exists' do
-    expect(@enigma).to be_a(Engima)
+    expect(@enigma).to be_a(Enigma)
   end
 end
