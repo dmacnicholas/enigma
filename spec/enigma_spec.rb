3710e361310458b17000ef4101dc7a2891ec7c22
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-11 14:47:18 -0500

    Test/todays date test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(@enigma).to be_a(Enigma)
   end
+
+  it 'can return todays date as DDMMYY format' do
+    expect(@enigma.todays_date).to_be(String)
+    expect(@enigma.todays_date.length).to eq(6)
+  end
 end
