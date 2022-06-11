1db22a266005e199f8a3ba723a53ae1489fc25e8
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-11 17:53:42 -0500

    Add in generate test and method

--- lib/key.rb
+++ lib/key.rb
@@ -1,5 +1,13 @@
 class Key
+attr_reader :key
+  def initialize(key)
+    @key = key
+  end
 
-  def initialize
+  def generate
+    key.chars.each_cons(2).map do |combo|
+      combo.join
+    end
+  end
 
 end
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -2,10 +2,18 @@ require './spec_helper'
 
 RSpec.describe Key do
   before :each do
-    @shift = Key.new
+    @key = Key.new("02715")
   end
 
   it 'exists' do
     expect(@key).to be_a(Key)
+  end
+
+  it 'returns a key' do
+    expect(@key.key).to eq("02715")
+  end
+
+  it 'can generate a key' do
+    expect(@key.generate).to eq(["02", "27", "71", "15"])
   end
 end
