612d9b80b2d8b4cbc0cfaeec23befeb222e56441
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-11 20:00:33 -0500

    Add in offset calc test and method

--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe Offset do
   it 'can return a date as DDMMYY format' do
     expect(@offset.date.length).to eq(6)
   end
+
+  it 'can return an array of the offset values' do
+    expect(@offset.offset_calc.length).to eq(4)
+  end
 end
