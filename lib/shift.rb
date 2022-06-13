2938c71b823d41b28494c63168d54f278d4a61b2
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-12 23:07:20 -0500

    Refactor shift methods for downcase

--- lib/shift.rb
+++ lib/shift.rb
@@ -18,7 +18,7 @@ class Shift
   end
 
   def forward(message)
-    message.chars.each_with_index.map do |letter, index|
+    message.downcase.chars.each_with_index.map do |letter, index|
       final_shift_index = index % 4 #index to use for final_shift
       shift_to_use = final_shift[final_shift_index] #number to add to current letter
       new_index = (shift_to_use + alphabet.index(letter)) % 27 #gives you index for the encrypted letter
@@ -27,7 +27,7 @@ class Shift
   end
 
   def backwards(encrypted_message)
-    encrypted_message.chars.each_with_index.map do |letter, index|
+    encrypted_message.downcase.chars.each_with_index.map do |letter, index|
       final_shift_index = index % 4 #index to use for final_shift
       shift_to_use = -(final_shift[final_shift_index]) #number to add to current letter
       new_index = (shift_to_use + alphabet.index(letter)) % 27 #gives you index for the encrypted letter
